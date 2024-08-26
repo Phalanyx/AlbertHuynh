a21dcc67c9ad26293b71e92a9cb398843620d6fa
Albert Huynh <itsalbert.huynh@mail.utoronto.ca>
2024-08-26 14:23:22 -0400

    contact page

--- my-app/src/components/Contact.js
+++ my-app/src/components/Contact.js
@@ -1,8 +1,17 @@
 import React from 'react'
-
+import './Contact.css'
 function Contact() {
   return (
-    <div>Contact</div>
+    <div className='contact'>
+      <div className='title'>
+      Get In Touch
+      </div>
+      <div className='contactBody'>
+           I’m currently looking for a full-time software engineering role and would love to connect with others who share a passion for technology and innovation.
+            If my work resonates with you, I’d be truly grateful for the opportunity to chat and explore potential collaborations!
+      </div>
+      <a href='mailto:itsalbert.huynh@mail.utoronto.ca'>Say Hi </a>
+    </div>
   )
 }
 
