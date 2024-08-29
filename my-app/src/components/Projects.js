33520a31d163660b79f1294ffd55a1186248932f
Albert Huynh <itsalbert.huynh@mail.utoronto.ca>
2024-08-28 23:50:11 -0400

    changed project style

--- my-app/src/components/Projects.js
+++ my-app/src/components/Projects.js
@@ -1,25 +1,45 @@
 import React from 'react'
-import { useState } from 'react'
+import { useState, useMemo } from 'react'
 import json from './ProjectData.json'
 import './Projects.css'
 
 function Projects() {
     const [onStart, setOnStart] = useState(true)
     const [data, setData] = useState([])
+    const [item, setItem] = useState(0)
 
     if (onStart) {
         setData(json['projects'])
         setOnStart(false)
     }
 
+    const leftClick = () => { 
+        if (item === 0) {
+            setItem(data.length - 1)
+        } else {
+            setItem(item - 1)
+        }
+    }
+    const rightClick = () => {  
+        if (item === data.length - 1) {
+            setItem(0)
+        } else {
+            setItem(item + 1)
+        }
+    }
+
+
     return (
         <div>
             <div id='project' className='projectHeader'>Projects</div>
             <ul className='projectTable'>
-                {data.map((project) => (
+                {data.map((project) => project.id === item ? (
                     <li className='projectItem' key={project.id}>
-                        <div className='image'>
-                            <img src={project.img} alt={project.name} />
+                        <div className='left' onClick={leftClick}> left</div>
+                        <div className='imgBorder'>
+                            <div className='image'>
+                                <img src={project.img} alt={project.name} />
+                            </div>
                         </div>
                         <div className='body'>
                             <a href={project.github} target="_blank" rel="noopener noreferrer">
@@ -27,8 +47,9 @@ function Projects() {
                             </a>
                             <p>{project.description}</p>
                         </div>
+                        <div className='right'onClick={rightClick}> right</div>
                     </li>
-                ))}
+                ) : <></>)}
             </ul>
         </div>
     )
